950efab7936cf5cb19c7bfde86ffe5e78f1407e1
yurashevtsov <yura.shevtsov92@gmail.com>
2025-01-10 11:27:18 +0300

    add user model

---
+++ src/user/userModel.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const { DataTypes, Model } = require("sequelize");
+
+// model needs to be connected to a database, from my understanding, which we pass in options
+const { sequelizeInstance } = require("@src/db/database.js");
+
+class User extends Model {}
+
+User.init(
+  //model properties
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: "username",
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: "email",
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue: "default.png",
+    },
+  },
+  // options
+  {
+    sequelize: sequelizeInstance,
+    modelName: "user",
+    tableName: "users",
+    timestamps: true, // to enable timestamps it must be set to true
+    updatedAt: true, // I want to enable updatedAt timestamp to for JWT verification, if user was updated after JWT was issued
+    createdAt: false,
+  }
+);
+
+module.exports = User;
