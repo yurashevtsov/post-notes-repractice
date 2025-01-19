075d194873abf2fcce8c1f511f5fa2593cb9d674
Yura Shevtsov <yura.shevtsov92@gmail.com>
2025-01-19 18:36:09 +0300

    added user login, signup, create, update validation

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -3,17 +3,39 @@
 const routerInstance = require("express").Router();
 const userController = require("./userController.js");
 const authMiddleware = require("@src/auth/authorization.middleware.js");
+const joiMiddleware = require("@src/middleware/joiMiddleware.js");
+const userValidationSchema = require("@src/user/userValidationSchema.js");
 
-routerInstance.post("/signup", userController.signup);
-routerInstance.post("/login", userController.login);
+routerInstance.post(
+  "/signup",
+  joiMiddleware.validateSchema(userValidationSchema.createUserSchema),
+  userController.signup
+);
 
-// applies to all routes below
+routerInstance.post(
+  "/login",
+  joiMiddleware.validateSchema(userValidationSchema.loginSchema),
+  userController.login
+);
+
+// AUTHORIZATION MIDDLEWARE
+// APPLIES TO ALL ROUTES BELOW
 routerInstance.use(authMiddleware.tokenAuthHandler);
 
 routerInstance.get("/", userController.getAllUsers);
+
 routerInstance.get("/:id", userController.getOneUser);
-routerInstance.post("/", userController.createUser);
-routerInstance.put("/:id", userController.updateUser);
+
+routerInstance.post(
+  "/",
+  joiMiddleware.validateSchema(userValidationSchema.createUserSchema),
+  userController.createUser
+);
+
+routerInstance.put("/:id",
+	joiMiddleware.validateSchema(userValidationSchema.updateUserSchema),
+	userController.updateUser);
+
 routerInstance.delete("/:id", userController.deleteUser);
 
 module.exports = routerInstance;
--- src/user/userValidationSchema.js
+++ src/user/userValidationSchema.js
@@ -2,18 +2,29 @@
 
 const Joi = require("joi");
 
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
 // with("field1", "field2") Requires the presence of other keys whenever the specified key is present.
 const createUserSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string().email(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   repeatPassword: Joi.ref("password"),
-}).with("password", "repeat_password");
+  avatar: Joi.string().optional(),
+}).with("password", "repeatPassword");
 
-// const updateUserSchema = Joi.object({});
-
-// const deleteUserSchema = Joi.object({});
+const updateUserSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).optional(),
+  avatar: Joi.string().optional(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).optional(),
+  repeatPassword: Joi.ref("password"),
+}).with("password", "repeatPassword");
 
 module.exports = {
+  loginSchema,
   createUserSchema,
+  updateUserSchema,
 };
