4612ed08a36e2d5738156f322d01a6282b220f87
yurashevtsov <yura.shevtsov92@gmail.com>
2025-01-21 15:32:28 +0300

    refactor: removed unnecessary filter helper

--- src/note/noteController.js
+++ src/note/noteController.js
@@ -2,7 +2,6 @@
 
 const catchAsync = require("@src/utils/catchAsync.js");
 const noteService = require("@src/note/noteService.js");
-const helpers = require("@src/utils/helpers");
 
 // user must be authenticated to use these
 
@@ -13,8 +12,8 @@ async function getAllUserNotes(req, res) {
 }
 
 async function getUserNote(req, res) {
-  const userId = req.user.id; // const { userId } = req.user;
-  const noteId = req.params.id; // const { id: noteId } = req.params;
+  const userId = req.user.id; 
+  const noteId = req.params.id;
 
   res.status(200).send(await noteService.getUserNote(userId, noteId));
 }
@@ -23,27 +22,16 @@ async function createUserNote(req, res) {
   const userId = req.user.id;
   const userData = req.body;
 
-  // filter user input
-  // I dont want them to modify userId or set their own note id
-  // will fix it with joi later
-  helpers.removeFieldsFromObj(req.body, ["userId", "id"]);
-
   res.status(201).send(await noteService.createUserNote(userId, userData));
 }
 
 async function updateUserNote(req, res) {
   const userId = req.user.id;
   const noteId = req.params.id;
-  // removing from object userid and id fields - these
-  const filteredUserData = helpers.keepAllowedFields(req.body, [
-    "name",
-    "color",
-    "description",
-  ]);
 
   res
     .status(200)
-    .send(await noteService.updateUserNote(userId, noteId, filteredUserData));
+    .send(await noteService.updateUserNote(userId, noteId, req.body));
 }
 
 async function deleteUserNote(req, res) {
